45e9e1c5898d589d0d633680eb456dcece530bd3
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-10 12:50:48 +0200

    QR Code generator

--- QR-code.py
+++ QR-code.py
@@ -1,16 +1,28 @@
 import qrcode
 
+# we use strip() to remove any leading or trailing whitespace from the input
 url = input("Enter the URL to generate QR code: ").strip() 
-# we used strip() to remove any leading or trailing whitespace from the input
-
 file_path = input("Enter the file path to save the QR code (e.g., 'qrcode.png'): ").strip()
 
+
+
+
+
+#here we can use just qr = qrcode.QRCode(), but we add some parameters to customize the QR code generation.
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
-)   
+)  
 qr.add_data(url)
 
+
+
 img = qr.make_image()
+img.save(file_path)
+
+
+print("---------------------------------------------")
+print(f"QR code generated and saved to {file_path}")
+print("---------------------------------------------")
